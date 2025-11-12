9087867ebebf79f09362c42543a6a47903078018
DayFerreira-rgb <eudayanesilvaferreira@gmail.com>
2025-11-12 16:46:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+///A lista ARRAY - Vetores
+
+var nomes = ["Beyoncé", "Rihanna", "Kendrick Lamar", "Milton Nascimento", "Roberto Carlos","Shawn Mendes", "Ivete Sangalo", "Ariana Grande", "Ed Sheeran", "Ferrugem", "Péricles", "Pablo Vittar", "Belo", "Orochi", "Filipe Ret", "Poze do Rodo"]
+
+var escolhaCantores = parseInt(prompt("Digite um número de 0 a 15"))
+
+
+console.log(nomes[escolhaCantores])
+document.write( "Nome:" + nomes [escolhaCantores])
